b99e7918d5dae4279374ddf536ea90be33290125
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-06 00:27:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(sec)
+  while sec > 0 do
+    if sec == 1
+      puts "#{sec} second!"
+    else
+      puts "#{sec} seconds!"
+    end
+    sec += 1
+  end
+  "HAPPY NEW YEAR!"
 end
